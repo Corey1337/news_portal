4325f55d6e7174c0d7c09d564cdb8302f8708356
unknown <kolinedr@gmail.com>
2021-06-19 19:58:29 +1000

    +article

---
+++ article.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Best news</title>
+</head>
+<body>
+    <?php require "blocks/header.php" ?>
+
+    <div class="container mt-5">
+    <h3 class="mb-5">Заголовок</h3>
+    <p><img src="img/1.jpg" class="leftimg img-thumbnail" width="20%">
+     Вчера во время проведения разведоперации наша группа. подверглась нападению неизвестного 
+     противника в камуфляжной форме Алиенов. В результате эффективной обороны. и стремительной 
+     контратаки многочисленная группа боевиков была смята и отброшена. Среди личного состава 
+     потерь нет. Бойцы разведгруппы проявили недюжие навыки владения оружием. Особо отличился 
+     в бою взводный Кудряшев;М.А., грамотно использовавший человеческие ресурсы 
+     своего взвода. В результате операции были захвачены элементы внеземной культуры, которые 
+     переданы аналитической группе.</p>
+    </div>
+
+    <?php require "blocks/footer.php" ?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>Best news</title>
 </head>
 <body>
@@ -13,31 +13,26 @@
 
     <div class="container mt-5">
         <h3 class="mb-5">Актуальные новости</h3>
-        <div>
+        <div class="d-flex flex-wrap">
             <?php
                 for($i = 0; $i < 3; $i++):
             ?>
-            <div class="col">
-                <div class="card mb-4 rounded-3 shadow-sm">
+            <div class="card mb-4 rounded-3 shadow-sm">
                 <div class="card-header py-3">
                     <h4 class="my-0 fw-normal">Заголовоки</h4>
                 </div>
                 <div class="card-body">
-                    <img src="img/<?php echo ($i + 1) ?>.jpg" class="img-thumbnail"></img>
-                    <ul class="list-unstyled mt-3 mb-4">
-                    <li>10 users included</li>
-                    <li>2 GB of storage</li>
-                    <li>Email support</li>
-                    <li>Help center access</li>
-                    </ul>
+                    <img src="img/<?php echo ($i + 1) ?>.jpg" class="img-thumbnail rounded mx-auto d-block" width="50%"></img>
+                    <div>
+                        <h5>Тут всякой текстик</h5>
+                    </div>
                     <button type="button" class="w-100 btn btn-lg btn-outline-primary">Подробнее</button>
                 </div>
             </div>
-        </div>
                  <?php endfor ?>
         </div>
     </div>
-    
+
     <?php require "blocks/footer.php" ?>
 </body>
 </html>
