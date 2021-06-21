9440cf65fcad02ff5d60f6d49cbe5496d5f0d6c9
Corey1337 <kolikendr@gmail.com>
2021-06-21 18:31:34 +1000

    backligth_fixes

--- personal_block/pa_blocks/pa_header.php
+++ personal_block/pa_blocks/pa_header.php
@@ -1,11 +1,13 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="../../js/cur_backligth.js"></script>
 <div class="row">
     <div class="col">
-        <a class="text-dark text-decoration-none" href="#">Мой профиль</a>
+        <a class="text-dark text-decoration-none" href="personal_area.php">Мой профиль</a>
     </div>
     <div class="col">
-        <a class="text-dark text-decoration-none" href="#">Настройки аккаунта</a>
+        <a class="text-dark text-decoration-none" href="account_set.php">Настройки аккаунта</a>
     </div>
     <div class="col">
-        <a class="text-dark text-decoration-none" href="#">Поддержка</a>
+        <a class="text-dark text-decoration-none" href="support.php">Поддержка</a>
     </div>
 </div>
