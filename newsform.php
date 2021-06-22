6d23c5c8f99e5a78486dac7b8d592c6a1238128b
Corey1337 <kolikendr@gmail.com>
2021-06-22 19:31:02 +1000

    pic_obt

--- newsform.php
+++ newsform.php
@@ -14,7 +14,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="css/style.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css">
-        <title>Best news</title>
+        <title>News Today</title>
     </head>
     <body>
         <?php require "blocks/header.php" ?>
