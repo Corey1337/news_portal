aeb8dd9f505a535f2cc05ddd2cd70a2d48456f91
karterrr <tagaev.a@bk.ru>
2021-06-23 01:01:11 +1000

    fixes

--- server/set_avatar.php
+++ server/set_avatar.php
@@ -10,26 +10,33 @@
 if(isset($_SESSION['id']))
 {
     if(isset($_POST['sub']))
-    {
+    {   $size = 1024000;
         $old_img=$_SESSION['img'];
-        //$result = mb_substr($old_img, 4);
-        $name = mt_rand(0, 10000) . $_FILES['file']['name'];
-        copy($_FILES['file']['tmp_name'], '../img/' . $name);
-        $login = $_SESSION['login'];
-        $result2 = mysqli_query ($db,"UPDATE `users_attribute` SET `img`='img/$name' WHERE `login`='$login'");
-        // Проверяем, есть ли ошибки
-        if ($result2=='TRUE')
+        if ($_FILES['picture']['size'] > $size)
         {
-            $_SESSION['img']='img/'.$name;
-            $file=$old_img;
-            unlink($_SERVER['DOCUMENT_ROOT'].'/'.$file);
-
-            //!!!!!ВОТ ТУТ СДЕЛАТЬ ПОП ИТ, ЧТО ПАРОЛЬ УСПЕШНО ИЗМЕНЁН
-            header('Location: /personal_block/account_set.php');
+            echo 'Давай файл поменьше';
         }
-        else 
+        else
         {
-            echo "Ошибка! Пароль не изменён";
+            //$result = mb_substr($old_img, 4);
+            $name = mt_rand(0, 10000) . $_FILES['file']['name'];
+            copy($_FILES['file']['tmp_name'], '../img/' . $name);
+            $login = $_SESSION['login'];
+            $result2 = mysqli_query ($db,"UPDATE `users_attribute` SET `img`='img/$name' WHERE `login`='$login'");
+            // Проверяем, есть ли ошибки
+            if ($result2=='TRUE')
+            {
+                $_SESSION['img']='img/'.$name;
+                $file=$old_img;
+                unlink($_SERVER['DOCUMENT_ROOT'].'/'.$file);
+
+                //!!!!!ВОТ ТУТ СДЕЛАТЬ ПОП ИТ, ЧТО ПАРОЛЬ УСПЕШНО ИЗМЕНЁН
+                header('Location: /personal_block/account_set.php');
+            }
+            else 
+            {
+                echo "Ошибка! Аватар не изменён";
+            }
         }
 
     }
