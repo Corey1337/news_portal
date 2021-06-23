3cd0dab1a773065b73690705e3eec221544e025d
Corey1337 <kolikendr@gmail.com>
2021-06-23 17:35:30 +1000

    vova's code fix

--- index.php
+++ index.php
@@ -42,26 +42,22 @@
         <!-- к этой кнопеке надо прикрутить проверку на админа хз как -->
 
         <div class="d-flex flex-wrap">
-                        <?php require "server/fun_spawn_news.php"; ?>
-                                    <?php  $result=get_news() ;
-                                
-                        while ($post= mysqli_fetch_assoc($result)) {
-            echo ' <div class="card mb-4 rounded-3 shadow-sm">
-                        <div class="card-header py-3">
-                                            <h4 class="my-0 fw-normal">';
-                                            print_r( $post['tittle']);
-                            echo'</h4>
-                                    <h6>';
-                            print(date('Y-m-d',strtotime($post['date'])));
-                                  echo'</h6>
-                         </div>
-                         <div class="card-body">
-                            <div> <img src="news_img/'; print_r($post['img_id'])  ;echo'.jpg" class=img-thumbnail rounded mx-auto d-block width=50% </img>
-                                 <h5>';print_r($post['intro_text']);echo'</h5>
-                             </div>
-                              <button type="button" class="w-100 btn btn-lg btn-outline-primary" onclick="document.location=`article.php`">Подробнее</button>
-                        </div>
-                        </div>';}?>
+        <?php require "server/fun_spawn_news.php"; 
+            $result=get_news();
+            while ($post= mysqli_fetch_assoc($result)) { ?>
+            <div class="card mb-4 rounded-3 shadow-sm">
+                <div class="card-header py-3">
+                    <h4 class="my-0 fw-normal"><?php print_r( $post['tittle']); ?></h4>
+                    <h6><?php print(date('Y-m-d',strtotime($post['date']))); ?></h6>
+                </div>
+                <div class="card-body">
+                    <div> <img src="news_img/<?php print_r($post['img_id']); ?>.jpg" class="img-thumbnail rounded mx-auto d-block" width="50%">
+                        <h5><?php print_r($post['intro_text']); ?></h5>
+                    </div>
+                    <button type="button" class="w-100 btn btn-lg btn-outline-primary" onclick="document.location='article.php'">Подробнее</button>
+                </div>
+            </div>
+            <?php }; ?>
         </div>
     </div>
 <!-- test -->
